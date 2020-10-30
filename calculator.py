880f612d5eeaf4dbd990b81d8f0209329f4d3d88
Vlad-Pirus <73012859+Vlad-Pirus@users.noreply.github.com>
2020-10-30 09:25:05 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a = input("Введіть перше число")
+b = input("Введіть друге число")
+c = input("Виберіть дію + - * / ")
+if c =="-":
+    print(int(a) - int(b))
+if c =="+":
+    print(int(a) + int(b))
+if c =="*":
+    print(int(a) * int(b))
+if c =="/":
+    print(int(a) / int(b))
+
---
+++ reverse.py
@@ -0,0 +1,6 @@
+name=input("Введіть текст")
+def reverse_string1(s):
+    return s[::-1]
+
+
+print(reverse_string1(name))
